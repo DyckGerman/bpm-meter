1f49124968f74cb5435eef3e06af780af3804a02
Unknown <dyckgerman@gmail.com>
2018-09-23 22:05:38 +0300

    fixed stream recreation logic

--- background.js
+++ background.js
@@ -10,22 +10,24 @@ chrome.runtime.sendMessage({
 });
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.type === msgTypes.POPUP_LOADED && !streamInitialized) {
+    if (message.type === msgTypes.POPUP_LOADED) {
         chrome.tabCapture.capture({
             audio: true
         }, (stream) => {
-            var ctx = new AudioContext();
-            analyser = ctx.createAnalyser();
-            var source = ctx.createMediaStreamSource(stream);
-            source.connect(analyser);
-            analyser.connect(ctx.destination);
-
-            analyser.fftSize = 2048;
-            fpoints = new Float32Array(2048);
-            analyser.getFloatFrequencyData(fpoints);
-            var a = 5;
-
-            streamInitialized = true;
+            if (!streamInitialized) {
+                var ctx = new AudioContext();
+                analyser = ctx.createAnalyser();
+                var source = ctx.createMediaStreamSource(stream);
+                source.connect(analyser);
+                analyser.connect(ctx.destination);
+    
+                analyser.fftSize = 2048;
+                fpoints = new Float32Array(2048);
+                analyser.getFloatFrequencyData(fpoints);
+                var a = 5;
+    
+                streamInitialized = true;
+            }
         });
 
         chrome.runtime.sendMessage({
